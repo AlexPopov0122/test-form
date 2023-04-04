7799bf48901f27c6a14f475f497b16f7ff4b19e2
Alexander <sahapopov2004@mail.ru>
2023-04-04 15:33:14 +0500

    test form

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.querySelector(".button");
+const field = document.querySelectorAll(".field");
+
+    field.forEach(el => {
+        el.addEventListener("focus", function() {
+            this.style.borderBottomColor = "#fff";
+        })
+    })
+
+    field.forEach(el => {
+        el.addEventListener("blur", function() {
+            this.style.borderBottomColor = "#3F4363";
+        })
+    })
+
+button.onclick = () => {
+    field.forEach(el => {
+        if(el.required) {
+        el.placeholder = "Обязательно*"
+    }
+    if(el.checkValidity()) {
+        el.style.borderBottomColor = "#3F4363";
+    } else {
+        el.style.borderBottomColor = "red";
+    }
+    })
+    if(!(/^[0-9]+$/.test(field[3].value))){
+        field[3].style.borderBottomColor = "red";
+    }
+}
